cb60e741f3c8fff4f5bd3131ba0d383bf2b09fc2
DATBOI <alexsimons9999@gmail.com>
2020-06-21 17:13:12 -0500

    Released Doki-Theme 8.0.4

--- buildSrc/DokiRelease.ts
+++ buildSrc/DokiRelease.ts
@@ -14,18 +14,16 @@ startRelease(async ({channel, versionNumber}) => {
   if (channel === 'all') {
     await readWriteTemplate("communityUpdatePluginTemplate.xml", communityDir, versionNumber);
   }
-  return runCommand(dokiThemeRepo, './ciScripts/preBuild.sh').then(() =>
-    runCommand(dokiThemeRepo, './ciScripts/buildPlugin.sh'))
+  return runCommand(dokiThemeRepo, './ciScripts/buildPlugin.sh')
     .then(() => {
       fs.copyFileSync(
         path.resolve(dokiThemeRepo, 'build', 'distributions', `doki-theme-jetbrains-${versionNumber}.zip`),
         path.resolve(communityAssetsDir, `doki-theme.${versionNumber}.zip`)
       )
     }).then(()=>
-      runCommand(dokiThemeRepo, './ciScripts/preBuild.sh').then(() =>
         runCommand(dokiThemeRepo, './ciScripts/buildPlugin.sh', {
           PRODUCT: 'ultimate'
-        })))
+        }))
     .then(()=>{
       fs.copyFileSync(
         path.resolve(dokiThemeRepo, 'build', 'distributions', `doki-theme-jetbrains-${versionNumber}.zip`),
