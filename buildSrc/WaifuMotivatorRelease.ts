5a254791f70cfa8e664a37f2cc3991bfd3adb3ac
Alex Simons <alexsimons9999@gmail.com>
2020-08-15 13:44:39 -0500

    Released Motivator 1.2.0-doki

---
+++ buildSrc/WaifuMotivatorRelease.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import fs from 'fs';
+import {canaryDir, communityDir, readWriteTemplate, runCommand, ultimateDir} from "./AssetTools";
+import {startRelease} from "./ReleaseTools";
+
+const themedComponentsDirectory = path.resolve(__dirname, '..', '..', 'waifu-motivator-plugin');
+const assetDirectory = path.resolve(__dirname, '..', 'waifu-motivator');
+
+startRelease(async ({channel, versionNumber}) => {
+    await readWriteTemplate("waifuMotivatorTemplate.xml", canaryDir, versionNumber);
+    await readWriteTemplate("waifuMotivatorTemplate.xml", ultimateDir, versionNumber);
+    if (channel === 'all') {
+      await readWriteTemplate("waifuMotivatorTemplate.xml", communityDir, versionNumber);
+    }
+    return runCommand(themedComponentsDirectory, './ciScripts/buildPlugin.sh')
+      .then(() => {
+        fs.copyFileSync(
+          path.resolve(themedComponentsDirectory, 'build', 'distributions', `waifu-motivator-plugin-${versionNumber}.zip`),
+          path.resolve(assetDirectory, `waifu-motivator.${versionNumber}.zip`)
+        )
+      });
+  })
