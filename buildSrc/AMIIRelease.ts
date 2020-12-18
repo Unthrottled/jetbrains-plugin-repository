c5bc3aa6f3986d01e55fe09a086fbd8f0faae40c
DATBOI <alexsimons9999@gmail.com>
2020-12-17 18:26:43 -0600

    Added AMII release pipeline

---
+++ buildSrc/AMIIRelease.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import fs from 'fs';
+import {canaryDir, communityDir, readWriteTemplate, runCommand, ultimateDir} from "./AssetTools";
+import {startRelease} from "./ReleaseTools";
+
+const amiiSourceDir = path.resolve(__dirname, '..', '..', 'AMII');
+const assetDirectory = path.resolve(__dirname, '..', 'amii');
+
+startRelease(async ({channel, versionNumber}) => {
+    await readWriteTemplate("amiiTemplate.xml", canaryDir, versionNumber);
+    await readWriteTemplate("amiiTemplate.xml", ultimateDir, versionNumber);
+    if (channel === 'all') {
+      await readWriteTemplate("amiiTemplate.xml", communityDir, versionNumber);
+    }
+    return runCommand(amiiSourceDir, './ciScripts/buildPlugin.sh', {
+      VERSION: versionNumber
+    })
+      .then(() => {
+        fs.copyFileSync(
+          path.resolve(amiiSourceDir, 'build', 'distributions', `AMII-${versionNumber}.zip`),
+          path.resolve(assetDirectory, `AMII.${versionNumber}.zip`)
+        )
+      });
+  })
