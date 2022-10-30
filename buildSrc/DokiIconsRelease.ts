7452574c21493965daeeb559e83aada4e3c131ad
Alex Simons <alexsimons9999@gmail.com>
2022-10-30 18:08:35 -0500

    Added Doki Icons Release

---
+++ buildSrc/DokiIconsRelease.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import fs from 'fs';
+import {canaryDir, command, communityDir, readWriteTemplate, runCommand, ultimateDir} from "./AssetTools";
+import {startRelease} from "./ReleaseTools";
+
+const dokiIconsSourceDir = path.resolve(__dirname, '..', '..', 'doki-theme-icons-jetbrains');
+const assetDirectory = path.resolve(__dirname, '..', 'doki-theme-icons');
+
+startRelease(async ({channel, versionNumber}) => {
+    await readWriteTemplate("dokiIconsTemplate.xml", canaryDir, versionNumber);
+    await readWriteTemplate("dokiIconsTemplate.xml", ultimateDir, versionNumber);
+    if (channel === 'all') {
+      await readWriteTemplate("dokiIconsTemplate.xml", communityDir, versionNumber);
+    }
+    return runCommand(dokiIconsSourceDir, command, {
+      VERSION: versionNumber
+    })
+      .then(() => {
+        fs.copyFileSync(
+          path.resolve(dokiIconsSourceDir, 'build', 'distributions', `Doki Theme Icons-${versionNumber}.zip`),
+          path.resolve(assetDirectory, `Doki Theme Icons.${versionNumber}.zip`)
+        )
+      });
+  })
